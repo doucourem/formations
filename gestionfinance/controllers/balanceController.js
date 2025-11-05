d09bde94a8743b28fdc98a2f1d25d67556e416d7
mistofsoundjata <mistofsoundjata2018@gmail.com>
2025-11-05 17:49:32 +0000

    hh
    hh

--- gestionfinance/controllers/balanceController.js
+++ gestionfinance/controllers/balanceController.js
@@ -1,6 +1,6 @@
 import pool from '../models/index.js';
 
 export const getBalanceHistory = async (req, res) => {
-  const result = await pool.query('SELECT * FROM balance_history WHERE user_id=$1', [req.user.id]);
+  const result = await pool.query('SELECT * FROM balance_history');
   res.json(result.rows);
 };
